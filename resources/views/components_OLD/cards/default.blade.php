74c4d01befcb39f4d4aa4a42f15bf33e1ed39c84
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-07-13 23:26:13 +0200

    Removed old files

--- resources/views/components_OLD/cards/default.blade.php
+++
@@ -1,7 +0,0 @@
-@php
-    $classes = 'bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700';
-@endphp
-
-<div {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
-</div>
--- resources/views/components_OLD/main/border.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="hidden sm:block">
-    <div class="py-8">
-        <div class="border-t border-orange-500"></div>
-    </div>
-</div>
